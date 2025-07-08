0ce489f89bd3492fc208e7bbc0e0d73d4790c98b
Lacy Morrow <me@lacymorrow.com>
2025-07-07 22:36:05 -0400

    auto commit

--- scripts/env-config.ts
+++
@@ -1,5 +0,0 @@
-import nextEnv from "@next/env";
-const { loadEnvConfig } = nextEnv;
-
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
--- src/components/assets/icon.tsx
+++
@@ -1,5 +0,0 @@
-import { ShipkitIcon } from "@/components/assets/shipkit-icon";
-
-export const Icon = (props: React.ComponentProps<typeof ShipkitIcon>) => {
-  return <ShipkitIcon {...props} />;
-};
